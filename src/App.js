e22c934b4065878bad81b6205816ba43c3e67e46
Dave86dev <davidochando1986@gmail.com>
2023-01-13 10:29:42 +0100

    Estructura basica proyecto react inicializada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './common/Header/Header';
+import { Footer } from './common/Footer/Footer';
+import { Home } from './pages/Home/Home';
+import { Login } from './pages/Login/Login';
+import { Register } from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {/*Esta parte será fija, situada arriba de la parte cambiante*/}
+
+        <Header />
+      
+        <Routes>
+          {/*Esta parte será cambiante*/}
+
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+
+        </Routes>
+        {/*Esta parte será fija, situada debajo de la parte cambiante*/}
+      
+        <Footer />
+    
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/common/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Header.css';
+
+export const Header = () => {
+
+    return (
+        <div className='headerDesign'>
+            soy el header
+        </div>
+    );
+    
+};
